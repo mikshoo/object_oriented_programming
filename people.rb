0c937c01f85af1bf0e4e6bff6230acdebe3fa913
mikshoo <mikshoo@gmail.com>
2014-01-16 14:48:22 +0100

    person.rb

---
+++ people.rb
@@ -0,0 +1,34 @@
+class Person
+
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
+
+	def greeting
+		puts "Hi, my name is #{name}"
+	end
+end
+
+
+class Student < Person
+	def learn
+		puts "I get it!"
+end
+end
+
+class Instructor < Person
+	def teach
+		puts "Everything in Ruby is an Object"
+
+	end
+end
+
+instructor = Instructor.new ("Chris")
+student = Student.new ("Christina")
+
+instructor.greeting 
+student.greeting
+
+
